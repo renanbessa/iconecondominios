35e428dbc27a698295b59d73320228d30ee407b7
renanbessa <renan.oliveira.bessa@gmail.com>
2020-03-09 23:08:17 -0300

    adicionado a sessao dos servicos

--- wp-content/themes/iconecondo/page-home.php
+++ wp-content/themes/iconecondo/page-home.php
@@ -8,22 +8,57 @@
                     <h1>A escolha certa para as soluções do seu condomínio</h1>
                     <p>Conheça nossos serviços para as mais diversas áreas, especialmente
                     em portaria, limpeza e administração de condomínios.</p>
-                    <a class="btn" href="">Saiba mais</a>
+                    <a class="btn btn-primary" href="">Saiba mais</a>
                 </div>
             </div>
         </section>
 
         <section class="services">
             <div class="container">
-                <h1>Alguns de nossos serviços</h1>
                 <div class="services-general">
-                    <div class="services-item">
-                        <?php 
-                            if(is_active_sidebar('services-1')) {
-                                dynamic_sidebar('services-1' );
-                            }
-                        ?>
+                    <div class="services-about">
+                        <h3>Alguns dos nossos serviços</h3>
+                        <h2>O que fazemos?</h2>
+                        <p>Temos uma equipe qualificada e comprometida em superar as expectativas dos
+                        nossos clientes. Prezando sempre pela qualidade do atendimento, procurando sempre 
+                        inovar para prestar o melhor serviço com máxima eficiência.
+                        </p>
+                        <a class="btn btn-secondary budget" href="#">Orçamento aqui</a>
                     </div>
+                    <div class="services-itens">
+                        <div class="item-service">
+                            <div class="icon-service">
+                                ...
+                            </div>
+                            <h3>Administração do Condomínio</h3>
+                            <p>Realizar reuniões de assembleia, fiscalizar funcionários, entre outros.
+                                Além disso, tomar conta de toda a parte administrativa do empreendimento.
+                            </p>
+                        </div>
+                        <div class="item-service">
+                            <div class="icon-service">
+                                ...
+                            </div>
+                            <h3>Administração do Condomínio</h3>
+                            <p>Realizar reuniões de assembleia, fiscalizar funcionários, entre outros.
+                                Além disso, tomar conta de toda a parte administrativa do empreendimento.
+                            </p>
+                        </div>
+                        <div class="item-service">
+                            <div class="icon-service">
+                                ...
+                            </div>
+                            <h3>Administração do Condomínio</h3>
+                            <p>Realizar reuniões de assembleia, fiscalizar funcionários, entre outros.
+                                Além disso, tomar conta de toda a parte administrativa do empreendimento.
+                            </p>
+                        </div>
+                    </div>
+                        <?php// 
+                            //if(is_active_sidebar('services-1')) {
+                                //dynamic_sidebar('services-1' );
+                            //}
+                        //?>
                 </div>
             </div>
         </section>
